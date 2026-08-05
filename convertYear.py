7c24704db10ce68354ba7987b0f276d070a5f5a7
417-39-methawee <56944@yupparaj.ac.th>
2026-08-05 11:51:43 +0700

    Update convertYear.py

--- convertYear.py
+++ convertYear.py
@@ -1,10 +1,6 @@
 import streamlit as st
-2
 st.title("แอปพลิเคชั่นแปลงปี พ.ศ. เป็น ค.ศ.")
-3
-4
-bh_year=st.number_input("กรอกปี พ.ศ. ที่ต้องการแปลง", value=
-5
+
+bh_year=st.number_input("กรอกปี พ.ศ. ที่ต้องการแปลง", value=2562
 ce_year=bh_year-543
-6
-st.header(f"ปี ค.ศ. คือ : {ce_year}")
+st.header(f"ปี ค.ศ. คือ : 2019 {ce_year}")
