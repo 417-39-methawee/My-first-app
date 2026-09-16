777999b2d1987af5f5a62ae839e206f23c3089fd
417-39-methawee <56944@yupparaj.ac.th>
2026-09-16 11:45:58 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -131,7 +131,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2, ans3, ans4)
 
 st.divider()
 st.write("นางสาวเมทาวี คุณยศยิ่ง เลขที่ 39 ม.4/17")
